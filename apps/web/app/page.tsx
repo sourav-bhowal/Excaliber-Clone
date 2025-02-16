941340662f626c46cb5056fb01044f9486fd8367
Sourav Bhowal <souravbhowal15@gmail.com>
2025-02-17 00:00:17 +0530

    backend done

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-5xl font-bold bg-green-500 text-center p-5">
+      <h1 className="text-2xl font-bold bg-blue-500 text-center p-5">
         Hello, world!
       </h1>
     </div>
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -3,8 +3,4 @@ import sharedConfig from "@repo/tailwind-config";
 /** @type {import('tailwindcss').Config} */
 export default {
   ...sharedConfig,
-  content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
-  ],
-}
+};
